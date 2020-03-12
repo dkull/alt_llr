1bf64493eb528f61055e34bccaabeb359a06dcbc
Tanel Liiv <tanel.liiv@defendec.com>
2020-03-12 10:52:18 +0200

    log if gmp_to_gw fails

--- glue.zig
+++ glue.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const log = std.debug.warn;
+const stderr = std.debug.warn;
 
 const c = @import("c.zig");
 const gmp = c.gmp;
@@ -7,9 +7,13 @@ const gw = c.gw;
 
 pub fn gmp_to_gw(in: gmp.mpz_t, out: gw.gwnum, out_ctx: *gw.gwhandle) void {
     const word_size: usize = 4;
-    const buf_size: usize = 200000;
-    var buf: [buf_size]u32 = undefined; 
+    // FIXME: max stack size is a thing
+    const buf_size: usize = 1000000;
+    var buf: [buf_size]u32 = undefined;
     var count: usize = 0;
-    const written_count_or_error = gmp.mpz_export(&buf, &count, -1, word_size, 0, 0, &in);
+    const void_ = gmp.mpz_export(&buf, &count, -1, word_size, 0, 0, &in);
+    if (count < 0) {
+        stderr("gmp_to_gw copy error {}\n", .{count});
+    }
     gw.binarytogw(out_ctx, &buf, @intCast(u32, count), out);
 }
